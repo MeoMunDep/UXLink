1faa14a32542b0437ba643bc57978f469f3bf7c0
BlackCat <184516521+MeoMunDep@users.noreply.github.com>
2025-01-13 00:30:36 +0700

    ...

--- UXLink/setup.sh
+++ UXLink/setup.sh
@@ -30,16 +30,10 @@ fi
 create_default_configs() {
     cat > configs.json << EOL
 {
-    "timeZone": "en-US",
-    "rotateProxy": false,
-    "skipInvalidProxy": false,
-    "proxyRotationInterval": 2,
-    "delayEachAccount": [5, 8],
-    "timeToRestartAllAccounts": 300,
-    "howManyAccountsRunInOneTime": 10,
-    "doTasks": true,
-    "playGames": true,
-    "referralCode": ""
+  "limit": 10,
+  "countdown": 300,
+  "country_time": "en-US",
+  "delayEachAccount": [1, 81]
 }
 EOL
 }
@@ -55,7 +49,7 @@ check_configs() {
 while true; do
     clear
     echo "============================================================================"
-    echo "    name BOT SETUP AND RUN SCRIPT"
+    echo "    UXLINK BOT SETUP AND RUN SCRIPT by @MeoMunDep"
     echo "============================================================================"
     echo
     echo "Current directory: $(pwd)"
@@ -115,7 +109,7 @@ while true; do
             else
                 print_green "Using node_modules from current directory"
             fi
-            node bot
+            node meomundep
             read -p "Press Enter to continue..."
             ;;
         4)
